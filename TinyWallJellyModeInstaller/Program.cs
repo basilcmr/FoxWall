cadbbfa11480092f447ae6fb5cd9ca46bca73de3
Dr.Fox <basilcmr@gmail.com>
2026-05-25 18:13:57 +0530

    Use taskkill & ServiceController; drop P/Invoke
    Remove low-level P/Invoke privilege/terminate code and KillTinyWallProcesses; instead invoke taskkill to terminate TinyWall.exe and use ServiceController to start the TinyWall service. Adjusted timing, simplified install/start flow (no automatic registry Add/Remove registration or running TinyWall.exe /install), and updated user-facing messages. Also updates build metadata (AssemblyInformationalVersion, sourcelink) and removes some Release build outputs.

--- TinyWallJellyModeInstaller/Program.cs
+++ TinyWallJellyModeInstaller/Program.cs
@@ -3,7 +3,6 @@ using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 using System.Reflection;
-using System.Runtime.InteropServices;
 using System.ServiceProcess;
 using System.Threading;
 using System.Windows.Forms;
@@ -12,120 +11,6 @@ namespace TinyWallJellyModeInstaller
 {
     static class Program
     {
-        [DllImport("advapi32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool OpenProcessToken(IntPtr ProcessHandle, uint DesiredAccess, out IntPtr TokenHandle);
-
-        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool LookupPrivilegeValue(string? lpSystemName, string lpName, out LUID lpLuid);
-
-        [DllImport("advapi32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool AdjustTokenPrivileges(IntPtr TokenHandle, [MarshalAs(UnmanagedType.Bool)] bool DisableAllPrivileges, ref TOKEN_PRIVILEGES NewState, uint BufferLength, IntPtr PreviousState, IntPtr ReturnLength);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        private static extern IntPtr OpenProcess(uint processAccess, bool bInheritHandle, int processId);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool TerminateProcess(IntPtr hProcess, uint uExitCode);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool CloseHandle(IntPtr hObject);
-
-        [StructLayout(LayoutKind.Sequential)]
-        private struct LUID
-        {
-            public uint LowPart;
-            public int HighPart;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        private struct LUID_AND_ATTRIBUTES
-        {
-            public LUID Luid;
-            public uint Attributes;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        private struct TOKEN_PRIVILEGES
-        {
-            public uint PrivilegeCount;
-            public LUID_AND_ATTRIBUTES Privilege;
-        }
-
-        private const uint TOKEN_ADJUST_PRIVILEGES = 0x0020;
-        private const uint TOKEN_QUERY = 0x0008;
-        private const uint SE_PRIVILEGE_ENABLED = 0x00000002;
-        private const string SE_DEBUG_NAME = "SeDebugPrivilege";
-        private const uint PROCESS_TERMINATE = 0x0001;
-
-        private static bool EnableDebugPrivilege()
-        {
-            IntPtr hToken;
-            if (!OpenProcessToken(Process.GetCurrentProcess().Handle, TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, out hToken))
-                return false;
-
-            try
-            {
-                LUID luid;
-                if (!LookupPrivilegeValue(null, SE_DEBUG_NAME, out luid))
-                    return false;
-
-                TOKEN_PRIVILEGES tp;
-                tp.PrivilegeCount = 1;
-                tp.Privilege.Luid = luid;
-                tp.Privilege.Attributes = SE_PRIVILEGE_ENABLED;
-
-                if (!AdjustTokenPrivileges(hToken, false, ref tp, 0, IntPtr.Zero, IntPtr.Zero))
-                    return false;
-
-                return Marshal.GetLastWin32Error() == 0;
-            }
-            finally
-            {
-                CloseHandle(hToken);
-            }
-        }
-
-        private static void KillTinyWallProcesses()
-        {
-            EnableDebugPrivilege();
-
-            foreach (var proc in Process.GetProcesses())
-            {
-                try
-                {
-                    if (proc.ProcessName.Equals("TinyWall", StringComparison.OrdinalIgnoreCase))
-                    {
-                        // Try native kill first with PROCESS_TERMINATE
-                        IntPtr hProc = OpenProcess(PROCESS_TERMINATE, false, proc.Id);
-                        if (hProc != IntPtr.Zero)
-                        {
-                            try
-                            {
-                                TerminateProcess(hProc, 0);
-                            }
-                            finally
-                            {
-                                CloseHandle(hProc);
-                            }
-                        }
-
-                        // Also try the framework's Kill as a backup
-                        try
-                        {
-                            proc.Kill();
-                        }
-                        catch { }
-                    }
-                }
-                catch { }
-            }
-        }
-
         [STAThread]
         static void Main()
         {
@@ -137,7 +22,7 @@ namespace TinyWallJellyModeInstaller
                 // 1. Confirm with user before proceeding
                 var result = MessageBox.Show(
                     "This will install the custom-modified TinyWall with Jelly Mode.\n\n" +
-                    "It will stop/terminate the running firewall service, copy files to 'C:\\Program Files (x86)\\TinyWall', register it in Windows, and start it.\n\n" +
+                    "It will stop the firewall service, copy files to 'C:\\Program Files (x86)\\TinyWall', and restart it.\n\n" +
                     "Do you want to proceed?",
                     "TinyWall Jelly Mode Installer",
                     MessageBoxButtons.YesNo,
@@ -149,13 +34,27 @@ namespace TinyWallJellyModeInstaller
 
                 string destDir = @"C:\Program Files (x86)\TinyWall";
                 
-                // 2. Kill all TinyWall processes (tray, controller, and service) using native debugger privileges
-                KillTinyWallProcesses();
+                // 2. Stop TinyWall Service and terminate tray processes using taskkill to fully unlock files
+                try
+                {
+                    var psi = new ProcessStartInfo
+                    {
+                        FileName = "taskkill.exe",
+                        Arguments = "/F /IM TinyWall.exe",
+                        CreateNoWindow = true,
+                        UseShellExecute = false
+                    };
+                    using (var p = Process.Start(psi))
+                    {
+                        p?.WaitForExit(5000);
+                    }
+                }
+                catch { }
 
-                // Wait two seconds to ensure files are fully unlocked
-                Thread.Sleep(2000);
+                // Wait another second to ensure files are fully unlocked
+                Thread.Sleep(1500);
 
-                // 3. Extract embedded ZIP resource
+                // 4. Extract embedded ZIP resource
                 string tempZipPath = Path.Combine(Path.GetTempPath(), "TinyWallFiles.zip");
                 
                 // Read from embedded resources
@@ -174,7 +73,7 @@ namespace TinyWallJellyModeInstaller
                     }
                 }
 
-                // 4. Unzip to C:\Program Files (x86)\TinyWall (overwriting)
+                // 5. Unzip to C:\Program Files (x86)\TinyWall (overwriting)
                 if (!Directory.Exists(destDir))
                 {
                     Directory.CreateDirectory(destDir);
@@ -207,46 +106,19 @@ namespace TinyWallJellyModeInstaller
                 }
                 catch { }
 
-                // 5. Install/Register TinyWall Service using its own built-in installer
+                // 6. Start TinyWall Service
                 try
                 {
-                    string twExePath = Path.Combine(destDir, "TinyWall.exe");
-                    var startInfo = new ProcessStartInfo
+                    using (var sc = new ServiceController("TinyWall"))
                     {
-                        FileName = twExePath,
-                        Arguments = "/install",
-                        UseShellExecute = true,
-                        Verb = "runas"
-                    };
-                    using (var p = Process.Start(startInfo))
-                    {
-                        p?.WaitForExit(10000);
+                        sc.Start();
+                        sc.WaitForStatus(ServiceControllerStatus.Running, TimeSpan.FromSeconds(10));
                     }
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show($"Warning: TinyWall Service could not be registered automatically: {ex.Message}", "Registration Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    MessageBox.Show($"Warning: TinyWall Service could not be started automatically: {ex.Message}\nYou can start it manually inside Services.msc.", "Service Start Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 }
-
-                // 6. Register in Windows Add/Remove Programs (Programs and Features)
-                try
-                {
-                    using (var key = Microsoft.Win32.Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\TinyWall"))
-                    {
-                        if (key != null)
-                        {
-                            key.SetValue("DisplayName", "TinyWall (Jelly Mode)");
-                            key.SetValue("UninstallString", $"\"{Path.Combine(destDir, "TinyWall.exe")}\" /uninstall");
-                            key.SetValue("DisplayVersion", "3.4.1");
-                            key.SetValue("Publisher", "Károly Pados / pylorak");
-                            key.SetValue("DisplayIcon", $"\"{Path.Combine(destDir, "TinyWall.exe")}\",0");
-                            key.SetValue("InstallLocation", destDir);
-                            key.SetValue("NoModify", 1);
-                            key.SetValue("NoRepair", 1);
-                        }
-                    }
-                }
-                catch { }
 
                 // 7. Start TinyWall Controller (Tray App)
                 try
@@ -261,7 +133,7 @@ namespace TinyWallJellyModeInstaller
                 catch { }
 
                 MessageBox.Show(
-                    "TinyWall Custom Jelly Mode has been successfully installed and started!\n\nIt is now registered in your Windows Add/Remove Programs list.",
+                    "TinyWall Custom Jelly Mode has been successfully installed and started!\n\nEnjoy watching movies on your TV with JellyMode whitelisted!",
                     "Success",
                     MessageBoxButtons.OK,
                     MessageBoxIcon.Information
--- TinyWallJellyModeInstaller/obj/Release/net48/TinyWallJellyModeInstaller.AssemblyInfo.cs
+++ TinyWallJellyModeInstaller/obj/Release/net48/TinyWallJellyModeInstaller.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TinyWallJellyModeInstaller")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c517573ac30146bc66baf6dc1fb3b6f21ce0b28c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24c81c61050da1b869aa69b62ea2b6bc0c113e82")]
 [assembly: System.Reflection.AssemblyProductAttribute("TinyWallJellyModeInstaller")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TinyWallJellyModeInstaller")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
